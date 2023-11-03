ecf302d57bf05995f18a493113595c60968f569f
Karen <karenkgs@gmail.com>
2023-11-03 18:59:31 +1100

    feat(eventsourcing): create event

--- eventsourcing/event.go
+++ eventsourcing/event.go
@@ -1,4 +1,19 @@
 package eventsourcing
 
+import (
+	"Users/karen/Developer/Personal/go/strays/aggregates"
+	"github.com/google/uuid"
+	"time"
+)
+
+type EventType string
+
 type Event struct {
+	id            uuid.UUID
+	aggregateType aggregates.AggregateType
+	aggregateID   uuid.UUID
+	position      int
+	eventType     EventType
+	Payload       interface{}
+	RecordedAt    time.Time
 }
